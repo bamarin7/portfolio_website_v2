dd2f7f2f9e0cfbb3cbe536a7b6550637eed9ca86
Brian Marin Silva <marinbrian.as@gmail.com>
2023-04-24 23:14:59 -0500

    Fixing bugs

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -16,7 +16,7 @@ export const Img2 = styled.img`
 
 export const ImageContainer = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
   height: auto;
   width: auto;
