8b7d5995172331eebafd4037e595d18d55ed7daa
Brian Marin Silva <78771390+bamarin7@users.noreply.github.com>
2023-04-24 19:22:50 -0500

    Add files via upload
    Fixing image size and position problems

---
+++ Hero/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+import useWindowSize from '../../hooks/useWindowSize';
+import { Img2, ImageContainer } from '../Projects/ProjectsStyles';
+
+
+const Hero = (props) => {
+  const { width } = useWindowSize();
+  const smBreakpoint = 640;
+
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome to <br /> My Portfolio! 👋🏼
+        </SectionTitle>
+        <SectionText>
+          My name is Brian Marin Silva and I'm a Full Stack Software Developer working on the MERN stack <br /><br /> I have a past in the Design/Advertising industry and I love building next-gen tools for the web
+        </SectionText>
+        <Button onClick={() => (window.location = '#projects')}>Learn More</Button>
+      </LeftSection>
+      {width <= smBreakpoint && (
+        <ImageContainer>
+          <Img2 src="/images/avatar.png" alt="avatar picture" />
+        </ImageContainer>
+      )}
+    </Section>
+  );
+};
+
+export default Hero;
